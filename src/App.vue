d33836bf38bda80636bf0895ba2331f32c5cd9d5
Iov Mihai <97.mihairazvan@gmail.com>
2023-02-27 21:12:12 +0200

    s

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,55 @@
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
-  import {onMounted} from 'vue'
-  import * as THREE from 'three'
-  onMounted(() => {
-    // Create a Three.js scene
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    let renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    // Add a cube to the scene
-    let cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-    let cubeMaterial = new THREE.MeshNormalMaterial();
-    let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    scene.add(cube);
-
-    // Set up the AR environment
-    navigator.xr.addEventListener('devicechange', checkXRSupport);
-    checkXRSupport();
-
-    async function checkXRSupport() {
-      const xrSupported = await navigator.xr.isSessionSupported('immersive-ar');
-      if (xrSupported) {
-        // Create a WebXR AR session
-        const xrSession = await navigator.xr.requestSession('immersive-ar');
-        const xrReferenceSpace = await xrSession.requestReferenceSpace('local');
-        const xrViewerPose = await xrSession.requestAnimationFrame(onXRFrame);
-
-        // Set the camera position based on the viewer pose
-        const position = xrViewerPose.transform.position;
-        const quaternion = xrViewerPose.transform.orientation;
-        camera.position.set(position.x, position.y, position.z);
-        camera.quaternion.set(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
-
-        // Render the scene
-        function onXRFrame(time, xrFrame) {
-          const pose = xrFrame.getViewerPose(xrReferenceSpace);
-          if (pose) {
-            const view = pose.views[0];
-            const viewport = xrSession.renderState.baseLayer.getViewport(view);
-            renderer.setSize(viewport.width, viewport.height);
-            renderer.setPixelRatio(window.devicePixelRatio);
-            xrSession.requestAnimationFrame(onXRFrame);
-            cube.position.set(0, 0, -1).applyMatrix4(view.transform.inverse.matrix);
-            renderer.render(scene, camera);
-          }
-        }
-      }
-    }
-  })
+  // import { RouterLink, RouterView } from 'vue-router'
+  // import {onMounted} from 'vue'
+  // import * as THREE from 'three'
+  // onMounted(() => {
+  //   // Create a Three.js scene
+  //   let scene = new THREE.Scene();
+  //   let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  //   let renderer = new THREE.WebGLRenderer({ antialias: true });
+  //   renderer.setSize(window.innerWidth, window.innerHeight);
+  //   document.body.appendChild(renderer.domElement);
+  //
+  //   // Add a cube to the scene
+  //   let cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  //   let cubeMaterial = new THREE.MeshNormalMaterial();
+  //   let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  //   scene.add(cube);
+  //
+  //   // Set up the AR environment
+  //   navigator.xr.addEventListener('devicechange', checkXRSupport);
+  //   checkXRSupport();
+  //
+  //   async function checkXRSupport() {
+  //     const xrSupported = await navigator.xr.isSessionSupported('immersive-ar');
+  //     if (xrSupported) {
+  //       // Create a WebXR AR session
+  //       const xrSession = await navigator.xr.requestSession('immersive-ar');
+  //       const xrReferenceSpace = await xrSession.requestReferenceSpace('local');
+  //       const xrViewerPose = await xrSession.requestAnimationFrame(onXRFrame);
+  //
+  //       // Set the camera position based on the viewer pose
+  //       const position = xrViewerPose.transform.position;
+  //       const quaternion = xrViewerPose.transform.orientation;
+  //       camera.position.set(position.x, position.y, position.z);
+  //       camera.quaternion.set(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
+  //
+  //       // Render the scene
+  //       function onXRFrame(time, xrFrame) {
+  //         const pose = xrFrame.getViewerPose(xrReferenceSpace);
+  //         if (pose) {
+  //           const view = pose.views[0];
+  //           const viewport = xrSession.renderState.baseLayer.getViewport(view);
+  //           renderer.setSize(viewport.width, viewport.height);
+  //           renderer.setPixelRatio(window.devicePixelRatio);
+  //           xrSession.requestAnimationFrame(onXRFrame);
+  //           cube.position.set(0, 0, -1).applyMatrix4(view.transform.inverse.matrix);
+  //           renderer.render(scene, camera);
+  //         }
+  //       }
+  //     }
+  //   }
+  // })
 </script>
 
 <template>
